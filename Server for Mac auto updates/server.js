d06921cda5b301ce2a5cfc2ac523cc7f34616c45
vinay-x <vinay.neekhra@xenovus.com>
2018-05-31 15:37:42 +0530

    Add server for auto updates for Mac app

---
+++ Server for Mac auto updates/server.js
@@ -0,0 +1,45 @@
+'use strict';
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(require('morgan')('dev'));
+
+app.use('/updates/releases', express.static(path.join(__dirname, 'releases')));
+
+app.get('/updates/latest', (req, res) => {
+    const latest = getLatestRelease();
+const clientVersion = req.query.v;
+
+if (clientVersion === latest) {
+    res.status(204).end();
+} else {
+    res.json({
+        url: `${getBaseUrl()}/releases/darwin/${latest}/MyApp.zip`
+    });
+}
+});
+
+let getLatestRelease = () => {
+    const dir = `${__dirname}/releases/darwin`;
+
+    const versionsDesc = fs.readdirSync(dir).filter((file) => {
+        const filePath = path.join(dir, file);
+    return fs.statSync(filePath).isDirectory();
+}).reverse();
+
+    return versionsDesc[0];
+}
+
+let getBaseUrl = () => {
+    if (process.env.NODE_ENV === 'development') {
+        return 'http://localhost:3000';
+    } else {
+        return 'http://download.mydomain.com'
+    }
+}
+
+app.listen(process.env.PORT, () => {
+    console.log(`Express server listening on port ${process.env.PORT}`);
+});
